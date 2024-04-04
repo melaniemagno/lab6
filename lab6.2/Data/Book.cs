3060b25a2c25fc3d69e64781caabe433dfd86925
Melanie Magno <MAGNOMK@ETSU.EDU>
2024-04-04 16:45:46 -0400

    Add project files.

---
+++ lab6.2/Data/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace lab6._2.Data
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
---
+++ lab6.2/Data/User.cs
@@ -0,0 +1,9 @@
+﻿namespace lab6._2.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
